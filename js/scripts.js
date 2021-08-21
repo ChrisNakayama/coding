2b0d90539ad4ed836966fb383c95d6cad8891be7
ChrisNakayama <88105917+ChrisNakayama@users.noreply.github.com>
2021-08-20 23:42:28 -0700

    final coding quiz

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,60 @@
-$(document).ready(function() {
-  $("button#green").click(function() {
-    $("body").removeClass();
-    $("body").addClass("green-background");
-  });
+var submitAnswer = function() {
 
-  $("button#yellow").click(function() {
-    $("body").removeClass();
-    $("body").addClass("yellow-background");
-  });
+  var radios = document.getElementsByName('choice');
+  var val= "";
+  for (var i = 0, length = radios.length; i < length; i++) {
+  if (radios[i].checked) {
+  val = radios[i].value; 
+  break;
+  }
+  }
+  
+if (val == "" ) {
+    alert('choose one');
+  } else if ( val == "Scripting" ) {
+    alert('Answer is correct !');
+  } else {
+    alert('Answer is wrong');
+  }
+};
 
-  $("button#red").click(function() {
-    $("body").removeClass();
-    $("body").addClass("red-background");
-  });
-});
+if (val == "" ) {
+  alert('choose one');
+} else if ( val == "useful" ) {
+  alert('Answer is correct !');
+} else {
+  alert('Answer is wrong');
+};
+
+if (val == "" ) {
+  alert('choose one');
+} else if ( val == "fun" ) {
+  alert('Answer is correct !');
+} else {
+  alert('Answer is wrong');
+};
+
+if (val == "" ) {
+  alert('choose one');
+} else if ( val == "Seattle" ) {
+  alert('Answer is correct !');
+} else {
+  alert('Answer is wrong');
+};
+
+if (val == "" ) {
+  alert('choose one');
+} else if ( val == "JavaScript Library" ) {
+  alert('Answer is correct !');
+} else {
+  alert('Answer is wrong');
+};
+
+if (val == "" ) {
+  alert('choose one');
+} else if ( val == "Ruby" ) {
+  alert('You should Learn Ruby!');
+} else {
+  alert('Answer is wrong');
+};
+
